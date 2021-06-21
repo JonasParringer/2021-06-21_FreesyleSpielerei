4ba92e942e3df1ab6c9a2a930718e35de3c7fb93
Jonas Parringer <jonas.parringer@drheuer.de>
2021-06-21 18:39:02 +0200

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_06_21_FreesyleChaosSpielerei.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
