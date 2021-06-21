d6d19cc26d6d24a4011a02754d2051162c525452
Jonas Parringer <jonas.parringer@drheuer.de>
2021-06-21 20:27:44 +0200

    Wie uebergeb ich jetzt Create an wen?

---
+++ Data/DocumentsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _2021_06_21_FreesyleChaosSpielerei.Models;
+
+    public class DocumentsContext : DbContext
+    {
+        public DocumentsContext (DbContextOptions<DocumentsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<_2021_06_21_FreesyleChaosSpielerei.Models.Document> Document { get; set; }
+    }
---
+++ Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2021_06_21_FreesyleChaosSpielerei.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public string Author { get; set; }
+
+        //[Display(Name = "date of publication")]
+        //[DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        // https://docs.microsoft.com/de-de/aspnet/core/tutorials/razor-pages/new-field?view=aspnetcore-5.0&tabs=visual-studio
+        public string DataType { get; set; }
+    }
+}
